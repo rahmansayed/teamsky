12878cb6769a393da9824c0fe030ed7209058e74
osamamohamed78 <osama.mohamed.78@gmail.com>
2016-11-19 16:59:34 +0300

    Add new file serverListHandler

--- www/js/services/serverListHandler.js
+++ www/js/services/serverListHandler.js
@@ -1,15 +1,17 @@
-angular.module('starter.services.serverlistHandler', [])
+angular.module('starter.services.serverListHandler', [])
 
-//TODO Create List
-// TODO Share List
-//TODO Check User Exist
+//TODO Move some code to Global area
 //TODO SYNCh List
-//TODO List Colour
+// TODO Unify the function names
+//TODO Invited user cannot delete the list
 
 
-  .factory('serverlistHandler', function ($http, global) {
+
+  .factory('serverListHandler', function ($http, global) {
 
     var lists = angular.fromJson(window.localStorage['lists'] || []);
+
+    var serviceName ="serverItemHandler=> ";
 
     function saveToLocalStorage() {
 
@@ -18,14 +20,17 @@ angular.module('starter.services.serverlistHandler', [])
 
     return {
       list: function () {
-
         return lists;
       },
-
+//------------------------getUserServerId
+      //TODO moved to global
+      getUserServerId: function () {
+        return "123";
+      },
 //------------------------getListId
+      //TODO moved to global
       getListId: function (listName) {
         for (var i = 0; i < lists.length; i++) {
-
           if (lists[i].listName === listName) {
             return lists[i];
           }
@@ -33,82 +38,111 @@ angular.module('starter.services.serverlistHandler', [])
         return undefined;
       },
 //------------------------checkInvitedUser
-
+      //TODO moved to global
       checkInvitedUser: function (contact) {
         data = {
           contact: contact
-         };
+        };
         console.log("data = "+data);
-        $http.post('http://' + global.serverIP + "/api/list/checkInvitedUser" , data)
+        $http.post(global.serverIP + "/api/list/checkInvitedUser" , data)
 
           .then(function (response) {
-            console.log('listhandler.addUser ' + response);
+            console.log('serverListHandler' + response);
           });
       },
 
 //------------------------createList
-      createList: function (listName,listLocalId) {
+      createList: function (listLocalId,listName,listDescription,listColour,listOrder) {
+        //userServerId=getUserServerId();
+
+        console.log(serviceName + "Start createList");
+
+
+        userServerId="582c3f6d30126504007c6bdf";
+        deviceServerId="582c3f6d30126504007c6be0";
+
         data = {
-          listName: listName,
           listLocalId:listLocalId,
-          userServerId:"123"
+          listName: listName,
+          listColour:listColour,
+          listOrder:listOrder,
+          userServerId:userServerId,
+          deviceServerId:deviceServerId
         };
 
-        console.log("data = "+data);
-        $http.post('http://' + global.serverIP + "/api/list/create" , data)
+        console.log(serviceName + "data = "+ JSON.stringify(data));
+
+        $http.post("http://"+global.serverIP + "/api/list/create" , data)
 
           .then(function (response) {
-            console.log('serverlistHandler' + response);
+            console.log(serviceName+" Response " + JSON.stringify(response));
           });
 
         //TODO update local DB with ServerID
 
       },
 //------------------------updateList
-      updateList: function (listName,newListName) {
+      updateList: function (listLocalId,ListName,listColour,listOrder,listSynch) {
 
-        listServerId=getListId(listName);
+        listServerId=123;
+        deviceServerId=123;
+
         data = {
           listServerId: listServerId,
-          newListName:newListName};
+          ListName:ListName,
+          deviceServerId:deviceServerId,
+          listColour:listColour,
+          listOrder:listOrder,
+          listSynch:listSynch
+        };
 
         console.log("data = "+data);
 
 
-        $http.post('http://' + global.serverIP + "/api/list/create" , data)
+        $http.post(global.serverIP + "/api/list/update" , data)
 
           .then(function (response) {
-            console.log('serverlistHandler' + response);
+            console.log('serverListHandler' + response);
           });
 
       },
 //------------------------deleteList
-      deleteList: function (listName) {
-        for (var i = 0; i < lists.length; i++) {
+      deleteList: function (listLocalId) {
+        listServerId=getListId(listLocalId);
+        deviceServerId=123;
 
-          if (lists[i].id === listId) {
-            lists.splice(i, 1);
-            saveToLocalStorage();
-            return;
-          }
-        }
-        ;
+        data = {
+          listServerId: listServerId,
+          deviceServerId:deviceServerId
+        };
+        console.log("data = "+data);
+
+        $http.post(global.serverIP + "/api/list/delete" , data)
+
+          .then(function (response) {
+            console.log('serverListHandler' + response);
+          });
+
       },
 //------------------------shareList
 
-      shareList: function (listServerId, invitedUserServerId) {
+      shareList: function (listLocalId,invitedUserServerId) {
+
+        listServerId=getListId(listLocalId);
+        deviceServerId=123;
         data = {
           invitedUserServerId: invitedUserServerId,
-          listServerId:listServerId
+          listServerId:listServerId,
+          deviceServerId:deviceServerId
         };
         console.log("data = "+data);
-        $http.post('http://' + global.serverIP + "/api/list/invite" , data)
+
+        $http.post(global.serverIP + "/api/list/share" , data)
 
           .then(function (response) {
-            console.log('listhandler.addUser ' + response);
+            console.log('serverListHandler ' + response);
           });
       }
-
 
     };
   });
