cff29e73d2af9d33e76dc245b5b5815df77943a7
abdurrahman <rahman.sayed@yahoo.com>
2017-03-08 13:20:32 +0300

    syncinit upon verifiation

--- www/js/controllers/verifyCtrl.js
+++ www/js/controllers/verifyCtrl.js
@@ -1,24 +1,24 @@
 angular.module('starter.controllers')
-  .controller('verifyCtrl', function ($scope, global, $http, $state,userVerify,dbHandler,global, $ionicHistory) {
+  .controller('verifyCtrl', function ($scope, global, $http, $state,userVerify, $ionicHistory, serverHandler) {
     $scope.settings = {};
     var data = {};
-    
+
       $ionicHistory.nextViewOptions({
     disableBack: true
   });
 
-    
-    
+
+
     user = userVerify.verificationData();
-    
-    
+
+
     $scope.userInfo=userVerify.selectedUser(user.deviceLocalId);
-    
+
     $scope.verify = function (vCode) {
         data = {
-        deviceLocalId: user.deviceLocalId,  
+        deviceLocalId: user.deviceLocalId,
         userServerId:user.userServerId,
-        deviceServerId:user.deviceServerId,  
+        deviceServerId:user.deviceServerId,
         vCode: vCode
       };
    /*  console.log('aalatief Verify, User Data:'+JSON.stringify($scope.verify));    */
@@ -29,14 +29,15 @@ angular.module('starter.controllers')
           alert(JSON.stringify(response.data));
            global.userServerId = response.data.userServerId;
            global.deviceServerId = response.data.deviceServerId;
-          
+
            console.log('06/02/2017 - verifyCtrl - aalatief after Verify: User Server ID:' + global.userServerId);
            console.log('06/02/2017 - verifyCtrl - aalatief after Verify: Device Server ID:' + global.deviceServerId);
           /*console.log('aaaltief: user to be updated:'+JSON.stringify(data)); */
           userVerify.updateUserInfo(data,'V');
           userVerify.addUserSetting(userInfo,'verified','Y');
           userVerify.addUserSetting(userInfo,'userServerId',response.data.userServerId);
-          userVerify.addUserSetting(userInfo,'deviceServerId',response.data.deviceServerId);                    
+          userVerify.addUserSetting(userInfo,'deviceServerId',response.data.deviceServerId);
+          serverHandler.syncInit();
 
           /*console.log('USER VERIFIED, User Data:'+JSON.stringify($scope.verify)); */
           //TODO go to lists only after succussfull verification
