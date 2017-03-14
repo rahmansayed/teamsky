27fc95ba6ee385b81043664519a65d9515099f69
abdurrahman <rahman.sayed@yahoo.com>
2017-03-14 16:20:46 +0300

    change server

--- www/js/services/global.js
+++ www/js/services/global.js
@@ -4,7 +4,7 @@ angular.module('starter.services')
     var settings = new Array();
     var dataKey;
     var serverIP = 'https://secret-savannah-80432.herokuapp.com';
-    var serverIP = 'http://129.0.89.36:4000';
+    //var serverIP = 'http://129.0.89.36:4000';
     //var serverIP = 'http://192.168.100.6:4000';
     //var serverIP = 'http://127.0.0.1:3000';
     var userName;
